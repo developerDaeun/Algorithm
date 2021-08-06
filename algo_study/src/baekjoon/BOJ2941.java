7866e19802a20bac4fe8e9472f7baf08490b31d9
developerDaeun <38871826+developerDaeun@users.noreply.github.com>
2021-08-06 15:15:55 +0900

    Update BOJ2941.java

--- algo_study/src/baekjoon/BOJ2941.java
+++ algo_study/src/baekjoon/BOJ2941.java
@@ -7,17 +7,17 @@ public class BOJ2941 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String s = br.readLine();
-		
-		for(int i = 0; i < s.length(); i++) {	// 크로아티아 문자가 있으면 모두 "0"으로 대체
-			s = s.replace("c=", "0");
-			s = s.replace("c-", "0");
-			s = s.replace("dz=", "0");
-			s = s.replace("d-", "0");
-			s = s.replace("lj", "0");
-			s = s.replace("nj", "0");
-			s = s.replace("s=", "0");
-			s = s.replace("z=", "0");
-		}
+		// 크로아티아 문자가 있으면 모두 "0"으로 대체	
+		s = s.replaceAll("c=|c-|dz=|d-|lj|nj|s=|z=", "0");
+//		for(int i = 0; i < s.length(); i++) {	
+//			s = s.replace("c-", "0");
+//			s = s.replace("dz=", "0");
+//			s = s.replace("d-", "0");
+//			s = s.replace("lj", "0");
+//			s = s.replace("nj", "0");
+//			s = s.replace("s=", "0");
+//			s = s.replace("z=", "0");
+//		}
 		System.out.println(s.length());
 	}
 }
