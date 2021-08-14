5202782e2ef790f7cde826d032643637d6d105e0
developerDaeun <38871826+developerDaeun@users.noreply.github.com>
2021-08-14 18:12:32 +0900

    Update BOJ1592_영식이와친구들.java

--- BOJ1592_영식이와친구들.java
+++ BOJ1592_영식이와친구들.java
@@ -19,21 +19,17 @@ public class BOJ1592_영식이와친구들 {
 		L = Integer.parseInt(st.nextToken());
 		getBall = new int[N];
 		
-		getBall[0] = 1;	// 1번 사람이 처음에 공을 받으므로 1 초기화
-		
-		bw.write(String.valueOf(game(0)));
+		bw.write(String.valueOf(game(0,0)));
 		bw.close();
 	}
 
-	static int game(int total) {
-		int cur = 0;
+	static int game(int cur, int total) {	// cur: 0번 시작, total: 총 공을 받은 횟수
 		while(getBall[cur] != M) {
+			getBall[cur]++; // 현재 받은 사람의 받은 횟수 추가
 			if(getBall[cur]%2==1) {	// 홀수번 공을 받았으면 시계방향으로 L번째 사람에게 공 던짐
 				cur = (cur+L)%N; // ex) N이 5일때, 3번째 사람이 2칸뒤인 0번째 사람에게 공을 던지려면 (3+2)%5 = 0번째 사람
-				getBall[cur]++;	
 			}else {	// 짝수번 공을 받았으면 반시계방향으로 L번째 사람에게 공 던짐
 				cur = ((N-L)+cur)%N; // ex) N이 5일때, 1번째 사람이 2칸 앞인 4번째 사람에게 공을 던지려면 ((5-2)+1)%5 = 4번째 사람
-				getBall[cur]++;	
 			}
 			total++;
 		}
