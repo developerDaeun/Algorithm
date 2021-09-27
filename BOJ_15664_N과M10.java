88da48048de845472ec730926d885725370972a1
developerDaeun <38871826+developerDaeun@users.noreply.github.com>
2021-09-28 00:53:21 +0900

    Add files via upload

---
+++ BOJ_15664_N과M10.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_15664_N과M10 {
+
+	static int N, M;
+	static int[] inputs;
+	static int[] numbers;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] s = br.readLine().split(" ");
+		
+		N = Integer.parseInt(s[0]);
+		M = Integer.parseInt(s[1]);
+		inputs = new int[N];
+		numbers = new int[M];
+		
+		s = br.readLine().split(" ");
+		for(int i = 0; i < N; i++) {
+			inputs[i] = Integer.parseInt(s[i]);
+		}
+		
+		Arrays.sort(inputs);	// 사전순 출력을 위해 오름차순 정렬
+		
+		perm(0, 0);
+		
+		System.out.print(sb);
+	}
+
+	static void perm(int cnt, int flag) {
+		if(cnt == M) {
+			for(int i = 0; i < M; i++) {
+				sb.append(numbers[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		int before = 0;
+		
+		for(int i = 0; i < N; i++) {
+			if((flag & 1<<i) != 0) continue;	// 사용중이면 continue
+			// 직전 수와 중복되지 않는지 체크
+			if(before == inputs[i])	// 직전수와 같으면 X
+				continue;
+			if(cnt>0 && numbers[cnt-1]>inputs[i]) continue;	// 앞의 숫자가 더 크면 continue
+			numbers[cnt] = inputs[i];
+			before = numbers[cnt];
+			perm(cnt+1, (flag | 1<<i));
+		}
+	}
+
+}
