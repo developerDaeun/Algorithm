bd7a164abc5c87f87832d509e99d3704355d4118
developerDaeun <38871826+developerDaeun@users.noreply.github.com>
2021-09-28 00:59:31 +0900

    Add files via upload

---
+++ BOJ_15665_N과M11.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_15665_N과M11 {
+
+	static int N, M;
+	static int[] inputs;
+	static int[] numbers;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] s = br.readLine().split(" ");
+		
+		N = Integer.parseInt(s[0]);
+		M = Integer.parseInt(s[1]);
+		inputs = new int[N];
+		numbers = new int[M];
+		
+		s = br.readLine().split(" ");
+		for(int i = 0; i < N; i++) {
+			inputs[i] = Integer.parseInt(s[i]);
+		}
+		
+		Arrays.sort(inputs);	// 사전순 출력을 위해 오름차순 정렬
+		
+		perm(0);
+		
+		System.out.print(sb);
+	}
+
+	static void perm(int cnt) {
+		if(cnt == M) {
+			for(int i = 0; i < M; i++) {
+				sb.append(numbers[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		int before = 0;
+		
+		for(int i = 0; i < N; i++) {
+			// 직전 수와 중복되지 않는지 체크
+			if(before == inputs[i])	// 직전수와 같으면 X
+				continue;
+			numbers[cnt] = inputs[i];
+			before = numbers[cnt];
+			perm(cnt+1);
+		}
+	}
+
+}
