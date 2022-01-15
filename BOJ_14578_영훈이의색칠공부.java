a483557e2af31027408dc521d289dc48d3b02549
ekdms4213 <38871826+ekdms4213@users.noreply.github.com>
2022-01-15 10:23:46 +0900

    update BOJ_14578_영훈이의 색칠공부

--- BOJ_14578_영훈이의색칠공부.java
+++ BOJ_14578_영훈이의색칠공부.java
@@ -1,12 +1,21 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/**
+ * 교란순열을 이해하고 풀어야 하는 문제
+ * 파란색을 먼저 칠했을 때 나올 수 있는 경우 중 대각선으로 칠했을 때 ⇒ n!
+ * 빨간색을 칠했을 때 ⇒ 빨간색 1개 칠한 값 + 2개 칠한 값
+ * 파란색이 있는 행 or 열에 빨간색 1개 칠했을 때 ⇒ (N-1)*f(N-1)
+ * 첫번째 열에 빨간색을 칠할 수 있는 칸 개수는 (N-1)
+ * 나머지 칸 개수는 f(N-1)
+ * 파란색이 없는 행 or 열에 빨간색 1개 칠했을 때 ⇒ (N-1)*f(N-2)
+ * 결과 : ( n! * (N-1) * (f(N-1) + f(N-2)) ) % 1,000,000,007
+*/
 public class BOJ_14578_영훈이의색칠공부 {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		// 1 <= N <= 10^5 ==> 10^5 * 10^5 == 100억개
 		int N = Integer.parseInt(br.readLine());
 
 		// 파란색을 칠했을 때 => n! 
@@ -16,13 +25,6 @@ public class BOJ_14578_영훈이의색칠공부 {
 			fact %= 1_000_000_007;
 		}
 		
-		// 파란색을 칠한 후, 빨간색을 칠했을 때 => 빨간색 1개 칠한 값 + 2개 칠한 값
-		
-		// 파란색이 있는 행 or 열에 빨간색 1개 칠했을 때 => (N-1)*f(N-1)
-		// 첫번째 열에 빨간색을 칠할 수 있는 칸 개수는 (N-1)
-		// 나머지 칸 개수는 f(N-1)
-		
-		// 파란색이 없는 행 or 열에 빨간색 1개 칠했을 때 => (N-1)*f(N-2)
 		long[] dp = new long[N + 1];
 		dp[0] = 1;
 		dp[1] = 0;
