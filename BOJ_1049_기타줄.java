580b946865ec77bd4f2cfef320e00e80ccb26a34
ekdms4213 <38871826+ekdms4213@users.noreply.github.com>
2022-02-08 00:25:38 +0900

    create BOJ_1198_삼각형으로자르기.java

--- BOJ_1049_기타줄.java
+++ BOJ_1049_기타줄.java
@@ -13,17 +13,21 @@ public class BOJ_1049_기타줄 {
         int m1 = Integer.MAX_VALUE;
         int m2 = Integer.MAX_VALUE;
         int res = Integer.MAX_VALUE;
+
+        // m1과 m2에 각각 기타줄 가격 최소값 구하기
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             m1 = Math.min(m1, Integer.parseInt(st.nextToken()));
             m2 = Math.min(m2, Integer.parseInt(st.nextToken()));
         }
 
+        // m1(기타줄 6개) 가격과 m2(낱개)*6 가격 비교
+        // m2*6 이 더 작거나 같다면, N개 만큼 가격 구하기
         if (m1 >= m2 * 6) {
             res = m2 * N;
-        } else {
+        } else {    // m1 가격이 더 크다면, 6개로 나눈 몫만큼 m1 곱하기
             res = N / 6 * m1;
-            if (N % 6 > 0) {
+            if (N % 6 > 0) {    // 6개로 나눈 나머지만큼 m1과 m2중 작은 값 더하기
                 res += Math.min(m1, N % 6 * m2);
             }
         }
