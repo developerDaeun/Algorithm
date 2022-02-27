39a9d3370d9072ce62f00717d57939a539cc2738
ekdms4213 <38871826+ekdms4213@users.noreply.github.com>
2022-02-27 18:13:14 +0900

    update test.md

---
+++ BOJ_16568_엔비스카의영혼.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_16568_엔비스카의영혼 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] s = br.readLine().split(" ");
+        int N = Integer.parseInt(s[0]);
+        int a = Integer.parseInt(s[1]);
+        int b = Integer.parseInt(s[2]);
+        int[] dp = new int[N + 1];
+
+        
+    }
+}
