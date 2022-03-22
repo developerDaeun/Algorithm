3920f25ee001725196fa89dd4c40976359c153b4
ekdms4213 <38871826+ekdms4213@users.noreply.github.com>
2022-03-22 23:47:44 +0900

    update PRO_42884_단속카메라.java

---
+++ PRO_42884_단속카메라.java
@@ -0,0 +1,16 @@
+public class PRO_42884_단속카메라 {
+    public static void main(String[] args) {
+
+        System.out.print(Solution.solution(new int[][]{{-20,-15}, {-14,-5}, {-18,-13}, {-5,-3}}));
+
+    }
+
+    static class Solution {
+        public static int solution(int[][] routes) {
+            int ans = 0;
+
+
+            return ans;
+        }
+    }
+}
