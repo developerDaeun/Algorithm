f67688abdae74deceaee117e3f9f595c2f3bb1f6
ekdms4213 <38871826+ekdms4213@users.noreply.github.com>
2022-03-25 23:48:17 +0900

    Update PRO_42895_N으로표현.java

---
+++ PRO_42895_N으로표현.java
@@ -0,0 +1,16 @@
+public class PRO_42895_N으로표현 {
+    public static void main(String[] args) {
+
+        System.out.print(Solution.solution(5, 12));
+
+    }
+
+    static class Solution {
+        public static int solution(int N, int number) {
+            int ans = 0;
+
+
+            return ans;
+        }
+    }
+}
