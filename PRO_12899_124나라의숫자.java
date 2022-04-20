7da3c010f7f019f858866f30cb59a82bcb018c81
ekdms4213 <38871826+ekdms4213@users.noreply.github.com>
2022-04-20 23:33:28 +0900

    Update PRO_12899_124나라의숫자.java

---
+++ PRO_12899_124나라의숫자.java
@@ -0,0 +1,15 @@
+public class PRO_12899_124나라의숫자 {
+    public static void main(String[] args) {
+        System.out.print(solution(1));
+    }
+
+    public static String solution(int n) {
+        String answer = "";
+
+        int num = 1;
+
+        num = n / 3;
+
+        return answer;
+    }
+}
