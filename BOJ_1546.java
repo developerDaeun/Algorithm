73c0a6da9de7e1ea792ad7322ee9ed2851627f10
pre pare <ekdms42132@gmail.com>
2022-09-07 04:56:13 +0000

    update BOJ_1546_평균

---
+++ BOJ_1546.java
@@ -0,0 +1,23 @@
+import java.util.StringTokenizer;
+import java.io.*;
+
+public class BOJ_1546 {
+    public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+     	int N = Integer.parseInt(br.readLine());
+        int[] score = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int max = 0;
+        for(int i = 0; i < N; i++){
+            score[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(max, score[i]);
+        }
+        
+        double total = 0;
+        for(int i = 0; i < N; i++){
+            total += (double)score[i] / max * 100;
+        }
+        
+        System.out.print((double)total / N);
+	}
+}
