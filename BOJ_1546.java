0799a4ae51ac3b44b709896605e80627f48c01c4
developerDaeun <null>
2022-09-07 06:31:12 +0000

    update BOJ_1546_평균

--- BOJ_1546.java
+++ BOJ_1546.java
@@ -11,7 +11,7 @@ public class BOJ_1546 {
         for(int i = 0; i < N; i++){
             score[i] = Integer.parseInt(st.nextToken());
             max = Math.max(max, score[i]);  
-        }
+        } 
         
         double total = 0;
         for(int i = 0; i < N; i++){
